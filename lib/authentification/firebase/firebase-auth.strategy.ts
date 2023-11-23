df7bc8e7e995c432b37cdac9a2de653041f5a491
Adrien Pingard <adrien.pingard@gmail.com>
2023-11-23 14:42:09 +0100

    fix(firebase config): change importation of the firebase config file

--- lib/authentification/firebase/firebase-auth.strategy.ts
+++ lib/authentification/firebase/firebase-auth.strategy.ts
@@ -1,21 +1,10 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Strategy, ExtractJwt } from 'passport-firebase-jwt';
-import * as firebaseConfig from './firebase.config.json';
 import * as firebase from 'firebase-admin';
+import { readFileSync } from 'fs';
 
-const firebaseParams = {
-  type: firebaseConfig.type,
-  projectId: firebaseConfig.project_id,
-  privateKeyId: firebaseConfig.private_key_id,
-  privateKey: firebaseConfig.private_key,
-  clientEmail: firebaseConfig.client_email,
-  clientId: firebaseConfig.client_id,
-  authUri: firebaseConfig.auth_uri,
-  tokenUri: firebaseConfig.token_uri,
-  authProviderX509CertUrl: firebaseConfig.auth_provider_x509_cert_url,
-  clientC509CertUrl: firebaseConfig.client_x509_cert_url,
-};
+const firebaseParams = JSON.parse(readFileSync('./firebase.config.json', 'utf8'));
 
 @Injectable()
 export class FirebaseAuthStrategy extends PassportStrategy(Strategy, 'firebase-auth') {
